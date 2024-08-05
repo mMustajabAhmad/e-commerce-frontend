9350f458dff6bad36fa6892122c3924b6a7c3c6f
SalihaShahid <salihashahid1102@gmail.com>
2024-08-05 12:05:13 +0500

    added footer in website

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,16 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 function HomePage(){
     return (
         <>
-            <Header />
+            <div className="wrapper">
+                <Header />
+                <main className="flex flex-col min-h-screen">
+                    <h1>This is body</h1>
+                </main>
+                <Footer className="mt-auto"/>
+            </div>
         </>
     );
 }
