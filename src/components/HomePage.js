24644fe160839af33044cd4ee93e8e14e68283aa
SalihaShahid <salihashahid1102@gmail.com>
2024-08-05 12:46:37 +0500

    made component for email subscription

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import EmailSubscription from "./EmailSubsription";
 
 function HomePage(){
     return (
@@ -10,14 +11,7 @@ function HomePage(){
                     <div style={{backgroundImage: "url('/images/product-photography-1.jpg')", height: "590px", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", opacity: "0.9"}} class="grid place-items-center">
 
                     </div>
-                    <div >
-                        <p className="font-bold text-5xl flex justify-center mt-6">Subscribe</p>
-                        <p className="text-gray-500 flex justify-center mt-4">Subscribe to our newsletter to receive news on update</p>
-                        <input className="px-10 py-2" type="email" placeholder="Your Email Address" style={{marginLeft: "42%", marginTop: "2%"}}></input>
-                        <br></br>
-                        <button className="bg-black text-white mt-6 pt-3 pb-3 pl-10 pr-10 mb-12 rounded hover:bg-purple-700" type="submit" style={{marginLeft: "45%"}}>Subscribe</button>
-                        <br></br>
-                    </div>
+                    <EmailSubscription />
                 </main>
                 <Footer className="mt-auto"/>
             </div>
