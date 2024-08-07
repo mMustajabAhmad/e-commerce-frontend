7fcd0bdeb69bb18874479c88e391f222dee48865
SalihaShahid <salihashahid1102@gmail.com>
2024-08-07 16:33:34 +0500

    added link for shop now button on home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import EmailSubscription from "./EmailSubsription";
 import ProductGridRow from "./ProductGridRow";
+import { Link } from "react-router-dom";
 
 function HomePage(){
     return (
@@ -17,7 +18,7 @@ function HomePage(){
                             <br></br>
                             <span class="text-white text-2xl flex justify-center">Shop your favorite products</span>
                             <br></br>
-                            <button type="submit" className="bg-white pl-10 pr-10 pt-4 pb-4 ml-20 mt-2 hover:bg-purple-700 hover:text-white">Shop Now</button>
+                            <button type="submit" className="bg-white pl-10 pr-10 pt-4 pb-4 ml-20 mt-2 hover:bg-purple-700 hover:text-white"><Link to="/shop">Shop Now</Link></button>
                         </div>
                     </div>
                     <div className="mb-12">
