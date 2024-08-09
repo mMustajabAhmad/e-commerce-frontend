3171b1b570cc2b77213e25568361457024472ef6
SalihaShahid <salihashahid1102@gmail.com>
2024-08-09 11:25:31 +0500

    added functionality to display  products dynamically on shop page

--- src/components/shop/ProductGridRow.js
+++ src/components/shop/ProductGridRow.js
@@ -1,10 +1,26 @@
 import ShopProduct from "./ShopProduct";
 
-function ProductGridRow(){
+function ProductGridRow(props){
+    const products=props.data;
+    const rows = [];
+    rows.push(
+        <div className="flex flex-col ">
+            <ShopProduct data={products ? products[0] : []}/>
+        </div>
+    );
+    for (let i=1; i<products.length; i++){
+        rows.push(
+            <div className="flex flex-col ml-4">
+                <ShopProduct data={products ? products[i] : []}/>
+            </div>
+        );
+    }
+
     return(
         <>
             <div className="flex flex-row mt-4">
-                <div className="flex flex-col ">
+                {rows}
+                {/* <div className="flex flex-col ">
                     <ShopProduct />
                 </div>
 
@@ -14,7 +30,7 @@ function ProductGridRow(){
 
                 <div className="flex flex-col ml-4">
                     <ShopProduct />
-                </div>
+                </div> */}
             </div>
         </>
     );
--- src/components/shop/RightPanel.js
+++ src/components/shop/RightPanel.js
@@ -18,16 +18,16 @@ function RightPanel(props){
 
     
     return (
-        <div>
-            <button type="button" className="border rounded  pl-4 pr-3 py-2 hover:bg-purple-700 hover:text-white w-1/4" onClick={handleClick} style={{marginTop: "13%"}}>
+        <div className="relative">
+            <button type="button" className="relative border rounded  pl-4 pr-3 py-2 hover:bg-purple-700 hover:text-white w-1/3" onClick={handleClick} style={{marginTop: "13%"}}>
                 default
                  <div className="float-right ml-12 mr-2"><i className='fa fa-angle-down '></i></div>
             </button>
             {menuClicked &&
-             <div className="border rounded mt-1 p-2 w-1/4">
+             <div className="border rounded mt-1 p-2 w-1/3 absolute bg-white">
                 <a href="#" className="hover:text-purple-700">Price High to Low</a>
                 <br></br>
-                <hr className="m-1"></hr>
+                <hr className="m-1 " ></hr>
                 <a href="#" className="hover:text-purple-700">Price Low to High</a>
                 <br></br>
                 <hr className="m-1"></hr>
