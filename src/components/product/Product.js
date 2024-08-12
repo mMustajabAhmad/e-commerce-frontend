821377e8ae5bfa5bdf9111deae83724acd04df01
SalihaShahid <salihashahid1102@gmail.com>
2024-08-12 13:49:56 +0500

    implemented code to handle product sizes dynamically

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,21 +4,69 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import apiClient from '../../api/authApi';
 
+
+
 function Product(){
     const { id } = useParams();
-    //console.log("id", id)
     const [product, setProduct] = useState(null);
-    const [imageURL, setImageURL] = useState(null)
+    const [imageURL, setImageURL] = useState(null);
+    const [sizes, setSizes] = useState(null);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [selectedSize, setSelectedSize] = useState(sizes ? sizes[0].name : "---");
+    const [priceOfSelectedSize, setPriceOfSelectedSize] = useState(sizes ? sizes[0].price : "---");
+    
+    useEffect(()=>{
+        const fetchSizes = async() => {
+            try{
+                const response = await apiClient.get(`/products/${id}/product_sizes`);
+                setSizes(response.data)
+            } catch(error){
+                console.log(error);
+            }
+        }
+        fetchSizes();  
+    },
+     []
+    );
     useEffect(()=>{
         const fetchProduct = async ()=>{
             const response = await apiClient.get(`/products/${id}`);
             setProduct(response.data);
             console.log("product",product);
-            setImageURL(response.data.product_images && response.data.product_images.length > 0 ? `http://localhost:3001/${response.data.product_images[0].url}` : '/images/watch1.png');
+            setImageURL(response.data.product_images && response.data.product_images.length > 0 ? `http://localhost:3001/${response.data.product_images[0].url}` : []);
         };
         fetchProduct();
     }, []
     );
+
+    function selectedPrams(size){
+        setSelectedSize(size.name);
+        setPriceOfSelectedSize(size.price)
+    }
+
+    const buttons = [];
+    if (sizes != null){
+        buttons.push(
+            <button onClick={()=> selectedPrams(sizes[0])} className="bg-purple-700 text-white border" type="button" style={{width: "40px", height: "40px"}} >{sizes? sizes[0].name : "XS"}</button>
+        );
+        for(let i=1; i<sizes.length; i++){
+            buttons.push(
+                <button onClick={()=> selectedPrams(sizes[i])} className= "bg-purple-700 text-white border ml-2"type="button" style={{width: "40px", height: "40px"}} >{sizes? sizes[i].name : "XS"}</button>
+            );
+        }    
+    }
+    function changeImage() {
+        if (product && product.product_images && product.product_images.length > 0) {
+            let nextImageIndex = currentImage + 1;
+            if (nextImageIndex >= product.product_images.length) {
+                nextImageIndex = 0;
+            }
+            setCurrentImage(nextImageIndex);
+            setImageURL(`http://localhost:3001/${product.product_images[nextImageIndex].url}`);
+            console.log("current image", nextImageIndex);
+        }
+    }
+
     
     return(
         <>
@@ -31,7 +79,7 @@ function Product(){
 
                     <div className="flex flex-row" style={{marginLeft: "10%", marginRight: "10%", marginTop: "8%", marginBottom: "4%"}}>
                         <div className="flex flex-col">
-                            <div className="rounded" style={{backgroundImage: `url(${imageURL})`, width: "470px", height: "550px", backgroundSize: "cover"}}></div>
+                            <div className="rounded" onClick={()=> changeImage()} style={{backgroundImage: `url(${imageURL})`, width: "470px", height: "550px", backgroundSize: "cover"}}></div>
                         </div>
                         <div className="flex flex-col" style={{marginLeft: "10%"}}>
                             {product ? 
@@ -43,15 +91,13 @@ function Product(){
                             <>
                             </>
                             }
-                            {/* <p className="text-2xl fornt-bold text-rose-600 mt-2">$ 500</p> */}
+                            <p className="text-2xl fornt-bold text-rose-600 mt-2">$ {priceOfSelectedSize}</p>
+                            <p className="text-1xl fornt-bold  mt-2"><b>Selected Size: </b><span className="text-rose-600">{selectedSize}</span></p>
+
                             <hr className="mt-4 ml-2 mr-6"></hr>
                             <p className="mt-4 font-bold text-2xl">Sizes</p>
                             <div className="flex flex-row mt-2">
-                                <button className="bg-purple-700 text-white border" type="button" style={{width: "40px", height: "40px"}}>XS</button>
-                                <button className= "bg-purple-700 text-white border ml-2"type="button" style={{width: "40px", height: "40px"}}>S</button>
-                                <button className="bg-purple-700 text-white border ml-2" type="button" style={{width: "40px", height: "40px"}}>M</button>
-                                <button className="bg-purple-700 text-white border ml-2" type="button" style={{width: "40px", height: "40px"}}>L</button>
-                                <button className="bg-purple-700 text-white border ml-2" type="button" style={{width: "40px", height: "40px"}}>XL</button>
+                                {sizes && buttons}
                             </div>
                             <button className="border bg-black text-white mt-6 hover:bg-purple-700" style={{height: "50px", width: "200px"}}>Add To Cart</button>
                         </div>
--- src/components/shop/ShopProduct.js
+++ src/components/shop/ShopProduct.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 function ShopProduct(props){
     const product = props.data;
     const imageURL = product.product_images && product.product_images.length > 0 ? `http://localhost:3001/${product.product_images[0].url}` : '/images/watch1.png'
+
+    
     return(
         <>
             <div>
