e161f81937eddc91f2a4f1ede52558870b5a9bbd
SalihaShahid <salihashahid1102@gmail.com>
2024-08-15 11:33:08 +0500

    added subcategories dynamically

--- src/components/categoryProductsAndSubCategories/CategoryProducts.js
+++ src/components/categoryProductsAndSubCategories/CategoryProducts.js
@@ -1,13 +1,16 @@
 import Header from "../shop/Header";
 import Footer from "../shop/Footer";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import CategoryProductRow from "./CategoryProductsRow";
 import { useEffect, useState } from "react";
 import apiClient from '../../api/authApi';
+import SubCategoryRow from "./SubCategoryRow";
 
 function CategoryProducts(){
     const { id } = useParams();
     const [products, setProducts] = useState(null);
+    const [subCategories, setSubCategories] = useState(null);
+    const [category, setCategory] =useState(null);
 
     useEffect(() =>{
         const fetchProducts = async ()=>{
@@ -15,10 +18,36 @@ function CategoryProducts(){
                 const response = await apiClient.get(`/categories/${id}/category_products`);
                 setProducts(response.data);
             }catch(error){
-                console.log("Error", error)
+                console.log("Error", error);
             }
         };
         fetchProducts();
+    }, []
+    );
+
+    useEffect(() =>{
+        const fetchCategory = async ()=>{
+            try{
+                const response = await apiClient.get(`/categories/${id}`);
+                setCategory(response.data);
+            }catch(error){
+                console.log("Error", error);
+            }
+        };
+        fetchCategory();
+    }, []
+    );
+
+    useEffect(() =>{
+        const fetchSubCategories = async ()=>{
+            try{
+                const response = await apiClient.get(`/categories/${id}/sub_categories`);
+                setSubCategories(response.data);
+            }catch(error){
+                console.log("Error", error);
+            }
+        };
+        fetchSubCategories();
     }, []
     );
 
@@ -26,57 +55,40 @@ function CategoryProducts(){
         <>
             <div className="mt-3">
                 <Header />
+
                 <main className="flex flex-col min-h-screen">
                     <div className="h-20 flex justify-center bg-gray-200 ">
-                        <span className="font-bold text-2xl mt-4">CATEGORY / NAME</span>
+                        <span className="font-bold text-2xl mt-4">SHOP / CATEGORY / {category && category.name.toUpperCase()}</span>
                     </div>
                     <div className="flex flex-col mt-6">
                         <div className="flex flex-row justify-center">
                             <div className="flex flex-col">
-                                <p className="text-4xl font-bold flex flex-row justify-center">Products</p>
-                                <div className="flex flex-row mt-6">
-                                    <CategoryProductRow data={products}/>
-                                </div>
+                                {products && products.length > 0 &&
+                                    <>
+                                        <p className="text-4xl font-bold flex flex-row justify-center">Products</p>
+                                        <div className="flex flex-row mt-6">
+                                            <CategoryProductRow data={products}/>
+                                        </div>
+                                    </>
+                                }
                             </div>
                         </div>
 
                         <div className="flex flex-row justify-center">
                             <div className="flex flex-col mt-12">
-                                <p className="text-4xl font-bold flex flex-row justify-center">Sub Categories</p>
-                                <div className="flex flex-row mt-6 mb-10">
-                                    <div style={{marginTop: "13%"}}>
-                                        <i className="fa fa-angle-double-left text-2xl hover:text-purple-700" ></i>
-                                    </div>
-                                    <div style={{backgroundImage: `url(/images/watch1.png)`, width: "350px", height: "350px", backgroundSize: "cover"}} className="ml-6">
-                                        <Link to="/categoryProducts">
-                                            <span className="flex justify-center text-white font-bold text-2xl hover:text-purple-700 hover:text-3xl" style={{marginTop: "40%"}}>
-                                                Category Name
-                                            </span>
-                                        </Link>
-                                    </div>
-                                    <div style={{backgroundImage: `url(/images/watch1.png)`, width: "350px", height: "350px", backgroundSize: "cover"}} className="ml-6">
-                                        <Link to="/categoryProducts">
-                                            <span className="flex justify-center text-white font-bold text-2xl hover:text-purple-700 hover:text-3xl" style={{marginTop: "40%"}}>
-                                                Category Name
-                                            </span>
-                                        </Link>
-                                    </div>
-                                    <div style={{backgroundImage: `url(/images/watch1.png)`, width: "350px", height: "350px", backgroundSize: "cover"}} className="ml-6">
-                                        <Link to="/categoryProducts">
-                                            <span className="flex justify-center text-white font-bold text-2xl hover:text-purple-700 hover:text-3xl" style={{marginTop: "40%"}}>
-                                                Category Name
-                                            </span>
-                                        </Link>
-                                    </div>
-                                    <div className="ml-4" style={{marginTop: "13%"}}>
-                                        <i className="fa fa-angle-double-right text-2xl hover:text-purple-700"></i>
-                                    </div>
-                                </div>
+                                {subCategories && subCategories.length > 0 &&
+                                    <>
+                                        <p className="text-4xl font-bold flex flex-row justify-center">Sub Categories</p>
+                                        <div className="flex flex-row mt-6 mb-10">
+                                            <SubCategoryRow data={subCategories}/>
+                                        </div>
+                                    </>
+                                }
                             </div>
                         </div>
                     </div>
                 </main>
-                
+
                 <Footer />
             </div>
         </>
---
+++ src/components/categoryProductsAndSubCategories/SubCategory.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+function SubCategory(props){
+    const subCategory = props.data;
+    const imageURL = subCategory && subCategory.category_image ? `http://localhost:3001/${subCategory.category_image.url}` : '/images/watch1.png';
+
+    
+    return(
+        <>
+            <div style={{backgroundImage: `url(${imageURL})`, width: "350px", height: "350px", backgroundSize: "cover"}} className="ml-6">
+                <a href={`/categoryProducts/${subCategory.id}`}>
+                    <span className="flex justify-center text-white font-bold text-2xl hover:text-purple-700 hover:text-3xl" style={{marginTop: "40%"}}>
+                        {subCategory.name}
+                    </span>
+                </a>
+            </div>
+        </>
+    );
+}
+
+export default SubCategory;
