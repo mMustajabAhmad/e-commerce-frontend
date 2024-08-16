01165503b93d753451c35fde685766bf2aefa665
SalihaShahid <salihashahid1102@gmail.com>
2024-08-16 15:39:52 +0500

    set default values of personal info

--- src/components/userProfile/AccountInfo.js
+++ src/components/userProfile/AccountInfo.js
@@ -1,4 +1,5 @@
-function AccountInfo(){
+function AccountInfo(props){
+    const user = props.data
     return(
         <>
             <div className='border border-gray-300 rounded flex flex-col'>
@@ -10,32 +11,25 @@ function AccountInfo(){
 
                 <form className='mx-6 mt-3'>
                     <div className='flex flex-row'>
-                        <div className='flex flex-col w-1/2 mr-4'>
-                            <label>First Name</label>
-                            <input type='text' className='border p-2 w-full'></input>
+                    <div className='flex flex-col w-full'>
+                            <label>Name</label>
+                            <input type='text' className='border p-2 w-full' value={user ? user.name : ""}></input>
                         </div>
-                        <div className='flex flex-col w-1/2 ml-4'>
-                            <label>Last Name</label>
-                            <input type='text' className='border p-2 w-full'></input>
-                        </div> 
+                        
                     </div>
 
                     <div className='flex flex-row mt-8'>
                         <div className='flex flex-col w-full'>
                             <label>Email Address</label>
-                            <input type='email' className='border p-2 w-full'></input>
+                            <input type='email' className='border p-2 w-full' value={user ? user.email : ""}></input>
                         </div>
                     </div>
 
                     <div className='flex flex-row mt-8'>
-                        <div className='flex flex-col w-1/2 mr-4'>
+                        <div className='flex flex-col w-full'>
                             <label>Contact Number</label>
-                            <input type='text' className='border p-2 w-full'></input>
+                            <input type='text' className='border p-2 w-full' value={user ? user.phone_number : ""}></input>
                         </div>
-                        <div className='flex flex-col w-1/2 ml-4'>
-                            <label>Contact Number(Alternate)</label>
-                            <input type='text' className='border p-2 w-full'></input>
-                        </div> 
                     </div>
                     <button type='submit' className='bg-black text-white py-3 px-8 float-right mb-8 mt-10 hover:bg-purple-700 font-bold'>CONTINUE</button>
                 </form>
--- src/components/userProfile/MyProfile.js
+++ src/components/userProfile/MyProfile.js
@@ -62,41 +62,6 @@ function MyProfile() {
                 <div className="h-20 flex justify-center bg-gray-200">
                     <span className="font-bold text-3xl mt-4">MY ACCOUNT</span>
                 </div>
-                {/* <div className="container mx-auto mt-6 px-4">
-                    <h1 className="text-3xl font-bold mb-4">Edit Account Information</h1>
-                    <div className="bg-white p-6 rounded-lg shadow-md">
-                        <h2 className="text-2xl font-semibold mb-4">User Information</h2>
-                        <form>
-                            <div className="mb-4">
-                                <label className="block text-gray-700">Name:</label>
-                                <input
-                                    type="text"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                    value={user?.name || ''}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700">Email:</label>
-                                <input
-                                    type="email"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                    value={user?.email || ''}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700">Phone Number:</label>
-                                <input
-                                    type="tel"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded"
-                                    value={user?.phone_number || ''}
-                                    readOnly
-                                />
-                            </div>
-                        </form>
-                    </div>
-                </div> */}
 
                 <div className='flex flex-col' style={{marginLeft: "20%",marginRight: "20%", marginTop: "7%"}}>
                     <div className='flex flex-row bg-gray-200 mt-4 border border-gray-300 rounded pl-6 py-6'>
@@ -114,7 +79,7 @@ function MyProfile() {
                     </div>
 
                     {editAccountInfo &&
-                        <AccountInfo />
+                        <AccountInfo data={user}/>
                     }
 
                     <div className='flex flex-row bg-gray-200 mt-4 border border-gray-300 rounded pl-6 py-6'>
