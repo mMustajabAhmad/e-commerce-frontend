ff133a0a8f1ac6664d8baca3544c973777a0b4cd
SalihaShahid <salihashahid1102@gmail.com>
2024-08-16 17:38:48 +0500

    integrated functionality to update user's personal info

--- src/components/userProfile/AccountInfo.js
+++ src/components/userProfile/AccountInfo.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import apiClient from '../../api/authApi';
+
 function AccountInfo(props){
-    const user = props.data
+    const user = props.data;
+    const [name, setName]=useState(user.name);
+    const [email, setEmail]=useState(user.email);
+    const [phoneNumber, setPhoneNumber]=useState(user.phone_number);
+
+    const handleNameChange = (event) =>{
+        setName(event.target.value);
+    }
+
+    const handleEmailChange = (event) =>{
+        setEmail(event.target.value);
+    }
+
+    const handlePhoneNumberChange = (event) =>{
+        setPhoneNumber(event.target.value);
+    }
+
+    const updateUserInfo = async() =>{
+        try{
+            await apiClient.put(`/users/${user && user.id}`,{"user":{"name": name, "email":email, "phone_number": phoneNumber}});
+        }catch(error){
+            console.log("ERROR", error);
+        }
+    };
+
+
     return(
         <>
             <div className='border border-gray-300 rounded flex flex-col'>
@@ -9,11 +37,11 @@ function AccountInfo(props){
                 </div>
                 <hr className='mb-4 mx-6'/>
 
-                <form className='mx-6 mt-3'>
+                <div className='mx-6 mt-3'>
                     <div className='flex flex-row'>
                     <div className='flex flex-col w-full'>
                             <label>Name</label>
-                            <input type='text' className='border p-2 w-full' value={user ? user.name : ""}></input>
+                            <input type='text' className='border p-2 w-full' value={name} onChange={handleNameChange}></input>
                         </div>
                         
                     </div>
@@ -21,18 +49,18 @@ function AccountInfo(props){
                     <div className='flex flex-row mt-8'>
                         <div className='flex flex-col w-full'>
                             <label>Email Address</label>
-                            <input type='email' className='border p-2 w-full' value={user ? user.email : ""}></input>
+                            <input type='email' className='border p-2 w-full' value={email} onChange={handleEmailChange}></input>
                         </div>
                     </div>
 
                     <div className='flex flex-row mt-8'>
                         <div className='flex flex-col w-full'>
                             <label>Contact Number</label>
-                            <input type='text' className='border p-2 w-full' value={user ? user.phone_number : ""}></input>
+                            <input type='text' className='border p-2 w-full' value={phoneNumber} onChange={handlePhoneNumberChange}></input>
                         </div>
                     </div>
-                    <button type='submit' className='bg-black text-white py-3 px-8 float-right mb-8 mt-10 hover:bg-purple-700 font-bold'>CONTINUE</button>
-                </form>
+                    <button  className='bg-black text-white py-3 px-8 float-right mb-8 mt-10 hover:bg-purple-700 font-bold' onClick={updateUserInfo}>UPDATE</button>
+                </div>
                 
             </div>
         </>
