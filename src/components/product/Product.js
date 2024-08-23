cb8648ee60437494568018c41cd1d0d2dd675a31
SalihaShahid <salihashahid1102@gmail.com>
2024-08-23 17:57:39 +0500

    partially refactored product.js

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,55 +4,88 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import apiClient from "../../api/authApi";
 import { getCurrentUserId } from "../../utils/JWT_TokenDecoder";
+import { fetchProductSizes, fetchProduct } from "../../utils/Product_APIs";
+import { useQuery } from "@tanstack/react-query";
 
 function Product() {
   const { id } = useParams();
-  const [product, setProduct] = useState(null);
-  const [imageURL, setImageURL] = useState(null);
-  const [sizes, setSizes] = useState(null);
+  // const [product, setProduct] = useState(null);
+  // const [imageURL, setImageURL] = useState(null);
+  // const [sizes, setSizes] = useState(null);
   const [currentImage, setCurrentImage] = useState(0);
-  const [selectedSize, setSelectedSize] = useState(sizes ? sizes[0] : null);
-  const [priceOfSelectedSize, setPriceOfSelectedSize] = useState(
-    sizes ? sizes[0].price : "---"
-  );
-  const [productSize, setProductSize] = useState(null);
+  // const [selectedSize, setSelectedSize] = useState(sizes ? sizes[0] : null);
+  // const [priceOfSelectedSize, setPriceOfSelectedSize] = useState(
+  //   sizes ? sizes[0].price : "---"
+  // );
+  // const [productSize, setProductSize] = useState(null);
   const user_id = getCurrentUserId();
 
-  useEffect(() => {
-    const fetchSizes = async () => {
-      try {
-        const response = await apiClient.get(`/products/${id}/product_sizes`);
-        setSizes(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchSizes();
-  }, []);
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const response = await apiClient.get(`/products/${id}`);
-      setProduct(response.data);
-      setImageURL(
-        response.data.product_images && response.data.product_images.length > 0
-          ? `http://localhost:3001/${response.data.product_images[0].url}`
-          : []
-      );
-    };
-    fetchProduct();
-  }, []);
+  // useEffect(() => {
+  //   const fetchSizes = async () => {
+  //     try {
+  //       const response = await apiClient.get(`/products/${id}/product_sizes`);
+  //       setSizes(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchSizes();
+  // }, []);
+  const {
+    data: sizes,
+    error: sizesError,
+    isLoading: loadingSizes,
+  } = useQuery({
+    queryKey: ["sizes", id],
+    queryFn: () => fetchProductSizes(id),
+  });
 
-  function selectedPrams(size) {
-    setSelectedSize(size);
-    setPriceOfSelectedSize(size.price);
-  }
+  const {
+    data: product,
+    error: productError,
+    isLoading: productIsLoading,
+  } = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => fetchProduct(id),
+  });
+
+  // useEffect(() => {
+  //   const fetchProduct = async () => {
+  //     const response = await apiClient.get(`/products/${id}`);
+  //     setProduct(response.data);
+  //     setImageURL(
+  //       response.data.product_images && response.data.product_images.length > 0
+  //         ? `http://localhost:3001/${response.data.product_images[0].url}`
+  //         : []
+  //     );
+  //   };
+  //   fetchProduct();
+  // }, []);
+
+  if (loadingSizes) return <div>Loading Sizes...</div>;
+  if (sizesError) return <div>Error in Loading Sizes</div>;
+
+  if (productIsLoading) return <div>Product is Loading...</div>;
+  if (productError) return <div>Error in Loading Product</div>;
+
+  const selectedSize = sizes[0];
+  const priceOfSelectedSize = sizes[0].price;
+  const imageURL =
+    product.product_images && product.product_images.length > 0
+      ? `http://localhost:3001/${product.product_images[0].url}`
+      : "images/watch1.png";
+
+  // function selectedPrams(size) {
+  //   setSelectedSize(size);
+  //   setPriceOfSelectedSize(size.price);
+  // }
 
   const buttons = [];
   if (sizes != null) {
     buttons.push(
       <button
         onClick={() => selectedPrams(sizes[0])}
-        className="bg-purple-700 text-white border"
+        className="bg-purple-700 text-white border rounded-md"
         type="button"
         style={{ width: "40px", height: "40px" }}
       >
@@ -159,7 +192,7 @@ function Product() {
         <Header />
         <main className="flex flex-col min-h-screen">
           <div className="h-20 flex justify-center bg-gray-200">
-            <span className="font-bold text-2xl mt-4">
+            <span className="font-medium text-2xl mt-4">
               HOME / SHOP / PRODUCT
             </span>
           </div>
@@ -179,7 +212,7 @@ function Product() {
                   <i className="fa fa-angle-double-left text-3xl"></i>
                 </button>
                 <div
-                  className="rounded"
+                  className="rounded-xl"
                   style={{
                     backgroundImage: `url(${imageURL})`,
                     width: "470px",
@@ -206,7 +239,7 @@ function Product() {
             <div className="flex flex-col" style={{ marginLeft: "10%" }}>
               {product ? (
                 <>
-                  <p className="text-3xl font-bold mt-6">{product.title}</p>
+                  <p className="text-3xl font-medium mt-6">{product.title}</p>
                   <p
                     className="flex justify-center mt-10 "
                     style={{ marginRight: "4px" }}
@@ -220,28 +253,28 @@ function Product() {
               <p className="text-2xl fornt-bold text-rose-600 mt-2">
                 $ {priceOfSelectedSize}
               </p>
-              <p className="text-1xl fornt-bold  mt-2">
-                <b>Selected Size: </b>
+              <p className="flex flex-row text-1xl font-medium  mt-2 gap-1">
+                Selected Size: 
                 <span className="text-rose-600">
                   {selectedSize && selectedSize.name}
                 </span>
               </p>
 
               <hr className="mt-4 ml-2 mr-6"></hr>
-              <p className="mt-4 font-bold text-2xl">Sizes</p>
+              <p className="mt-4 font-medium text-2xl">Sizes</p>
 
               <div className="flex flex-row mt-2">{sizes && buttons}</div>
 
               <div className="flex flex-row">
                 <button
-                  className="border bg-black text-white mt-6 hover:bg-purple-700"
+                  className="border rounded-lg bg-black text-white mt-6 hover:bg-purple-700"
                   style={{ height: "50px", width: "200px" }}
                   onClick={addToCart}
                 >
                   Add To Cart
                 </button>
                 <button
-                  className="border bg-black text-white mt-6 hover:bg-purple-700 ml-2"
+                  className="border rounded-lg bg-black text-white mt-6 hover:bg-purple-700 ml-2"
                   style={{ height: "50px", width: "200px" }}
                 >
                   <a href="https://wa.me/+923356517758" target="_blank">
--- src/utils/Product_APIs.js
+++ src/utils/Product_APIs.js
@@ -10,3 +10,21 @@ export const fetchProducts = async () => {
     console.log("Error", error);
   }
 };
+
+export const fetchProductSizes = async (product_id) =>{
+  try{
+    const response = await axios.get(`${API_BASE_URL}/products/${product_id}/product_sizes`);
+    return response.data;
+  }catch(error) {
+    console.log("Error", error);
+  }
+}
+
+export const fetchProduct = async (product_id) =>{
+  try{
+    const response = await axios.get(`${API_BASE_URL}/products/${product_id}`)
+    return response.data;
+  }catch(error){
+    console.log("Error", error);
+  }
+}
