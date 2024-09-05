e8809e7670bc53184dfc96c4d378801ab226fa88
SalihaShahid <salihashahid1102@gmail.com>
2024-09-05 17:47:56 +0500

    implmented conditional routing for checkout and payNow page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import MyProfile from "./components/userProfile/MyProfile";
@@ -15,8 +15,53 @@ import Checkout from "./components/checkout/Checkout";
 import SearchedProducts from "./components/searchResults/SearchedProducts";
 import StripeWrapper from "./components/checkout/Stripe";
 import OrderHistory from "./components/orderHistory/OrderHistory";
+import { fetchCart } from "./utils/APIs/Cart_APIs";
+import { useQuery } from "@tanstack/react-query";
+import { fetchOrderDetails } from "./utils/APIs/Order_APIs";
+
+const ConditionalRedirectForCheckout = () => {
+  const {
+    data: cartData,
+    error: cartError,
+    isLoading: cartIsLoading,
+  } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () => fetchCart(),
+  });
+  if (cartIsLoading) return <div>Loading Cart...</div>
+  if(cartError) return <div>Cart Error</div>;
+  if (cartData.cart.length > 0) {
+    return <Checkout/>;
+  } else {
+    return <Navigate to="/shop" />;
+  }
+};
+
+const ConditionalRedirectForPayment = () => {
+  const { order_id } = useParams();
+  const {
+    data: order,
+    error: orderError,
+    isLoading: orderIsLoading
+  } = useQuery({
+    queryKey: ["order", order_id],
+    queryFn: fetchOrderDetails(order_id)
+  })
+
+  if(orderIsLoading) return <div>Loading order...</div>
+  if(orderError) return <Navigate to= "/orderHistory"/>
+
+  if(order && order.payment_method == 'stripe'){
+    return <StripeWrapper />
+  }
+  else{
+    return <Navigate to="/orderHistory"/>
+  }
+}
+
 
 function App() {
+ 
   return (
     <Router>
       <Routes>
@@ -26,7 +71,6 @@ function App() {
           <>
             <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage />} />
-            <Route path="/shop" element={<Shop />} />
             <Route path="/myProfile" element={<MyProfile />} />
             <Route path="/products/:id" element={<Product />} />
             <Route path="/categories" element={<Categories />} />
@@ -34,10 +78,10 @@ function App() {
               path="/categoryProducts/:id"
               element={<CategoryProducts />}
             />
-
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout" element={<ConditionalRedirectForCheckout/>} />
+            <Route path="/shop" element={<Shop />} />
             <Route path="/searchedProducts/:query" element={<SearchedProducts />} />
-            <Route path="/payNow/:order_id" element={<StripeWrapper />} />
+            <Route path="/payNow/:order_id" element={<ConditionalRedirectForPayment />} />
             <Route path="/orderHistory" element={<OrderHistory />}/>
           </>
         ) : (
